606fd26a69940781760824703f3d1855d5d59faa
Diego Bentes <diegopbentes@gmail.com>
2017-06-06 18:47:28 -0300

    Add routes control

--- Uphp/web/Application.php
+++ Uphp/web/Application.php
@@ -1,6 +1,9 @@
 <?php
 namespace Uphp\web;
 
+use \UPhp\ActionDispach\Routes as Route;
+use \UPhp\ActionController\ActionController;
+
 class Application
 {
     public function __construct()
@@ -9,21 +12,29 @@ class Application
         set_error_handler("src\uphpErrorHandler");
     }
 
-    public function start()
+    public function start($config)
     {
        //carregando os initializers
         $this->getInitializersFiles();
+        $this->getRoutes();
+        ActionController::callController($config);
+
     }
 
     private function getInitializersFiles()
     {
         $path = "config/initializers/";
         $directory = dir($path);
-        while ( $file = $directory -> read() ) {
-            if ( $file != "." && $file != ".." ) {
+        while ($file = $directory -> read()) {
+            if ($file != "." && $file != "..") {
                 require($path . $file);
             }
         }
         $directory->close();
+    }
+
+    private function getRoutes()
+    {
+        return require("config/routes.php");
     }
 }
