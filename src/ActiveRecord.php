46580ea94bb44beb7f2a46b663b88131d96d8843
Renan Valente <renan.a.valente@gmail.com>
2017-07-17 18:50:03 -0300

    Updating folder structure and add file ActiveRecord.php

---
+++ src/ActiveRecord.php
@@ -0,0 +1,22 @@
+<?php
+namespace Uphp\web;
+
+/**
+ * ActiveRecord
+ * @package Uphp\web
+ * @link api.uphp.io
+ * @since 0.0.1
+ * @author Renan Valente <renan.a.valente@gmail.com>
+ */
+class ActiveRecord
+{
+    public function __construct() {
+        $config = require("config/application.php");
+        var_dump($config);die;
+
+        if ($drive == 'mysql')
+            return new \UPhp\MySql\ActiveRecord($model);
+        elseif ($drive == 'mongodb')
+            return new \UPhp\MongoDB\ActiveRecord($model);
+    }
+}
